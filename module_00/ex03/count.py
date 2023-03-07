a1f6acc1d8a60750d459ee4c87492b17077b0d55
noctuelles <plouvel@student.42.fr>
2023-03-07 18:58:22 +0100

    behavior in ex03

--- module_00/ex03/count.py
+++ module_00/ex03/count.py
@@ -1,31 +1,35 @@
 import string
 import sys
 
+
 def text_analyzer(text):
     """This function counts the number of upper characters, lower characters,
-       punctuation and spaces in a given text."""
+    punctuation and spaces in a given text."""
     sys.tracebacklimit = 0
-    assert type(text) is str and text is not None, ("argument is not a string.")
+    assert type(text) is str and text is not None, "argument is not a string."
     while text is None or len(text) == 0:
-        text = input('Type a text to be analyze : ')
+        text = input("Type a text to be analyze : ")
 
-    count = lambda l1,l2: sum(1 for x in l1 if x in l2)
+    count = lambda l1, l2: sum(1 for x in l1 if x in l2)
 
-    upper_case = count(text, string.ascii_uppercase);
-    lower_case = count(text, string.ascii_lowercase);
-    punc = count(text, string.punctuation);
-    space = count(text, " ");
+    upper_case = count(text, string.ascii_uppercase)
+    lower_case = count(text, string.ascii_lowercase)
+    punc = count(text, string.punctuation)
+    space = count(text, " ")
 
-    print(f"""The text contains {len(text)} character(s):
+    print(
+        f"""The text contains {len(text)} character(s):
 - {upper_case} upper letter(s)
 - {lower_case} lower letter(s)
 - {punc} punctuation mark(s)
-- {space} space(s)""")
+- {space} space(s)"""
+    )
+
 
 if __name__ == "__main__":
     sys.tracebacklimit = 0
-    assert len(sys.argv) <= 2, ("more than one argument is provided")
+    assert len(sys.argv) <= 2, "more than one argument is provided"
     if len(sys.argv) > 1:
         text_analyzer(sys.argv[1])
     else:
-        text_analyzer(None)
+        text_analyzer("")
