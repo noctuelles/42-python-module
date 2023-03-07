3df5e9072ad50d9562f59d984c7673aed7fa187b
noctuelles <plouvel@student.42.fr>
2023-03-07 19:04:14 +0100

    fixed ex02

--- module_00/ex02/whois.py
+++ module_00/ex02/whois.py
@@ -3,8 +3,12 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         sys.tracebacklimit = 0
-        assert len(sys.argv) != 2, ("more than one argument are provided")
-        assert type(sys.argv[1]) == int, ("argument is not an integer")
+        assert len(sys.argv) == 2, "more than one argument are provided"
+        assert (
+            sys.argv[1].isdigit()
+            if sys.argv[1][0] != "-"
+            else sys.argv[1][1:].isdigit()
+        ), "argument is not an integer"
 
         i = int(sys.argv[1])
         if i == 0:
