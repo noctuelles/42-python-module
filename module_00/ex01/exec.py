a3f0e4d43be1309adff3f78a7005b4afb6f6b86f
noctuelles <plouvel@student.42.fr>
2023-03-07 19:06:45 +0100

    header

--- module_00/ex01/exec.py
+++ module_00/ex01/exec.py
@@ -1,13 +1,27 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    exec.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/03/07 19:06:23 by plouvel           #+#    #+#              #
+#    Updated: 2023/03/07 19:06:24 by plouvel          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 import sys
+
 
 def main(argc, argv):
     for i, arg in enumerate(reversed(argv[1:])):
         reversed_arg = arg[::-1]
         first_arg_i = argc - 2
         print(
-                "".join(c.upper() if c.islower() else c.lower() for c in reversed_arg),
-                end=" " if i != first_arg_i else "\n"
-            )
+            "".join(c.upper() if c.islower() else c.lower() for c in reversed_arg),
+            end=" " if i != first_arg_i else "\n",
+        )
+
 
 if __name__ == "__main__":
     main(len(sys.argv), sys.argv)
