78af21518a660afc031e973af9214660eda5ce88
noctuelles <plouvel@student.42.fr>
2023-03-08 15:42:03 +0100

    ex00

--- module_01/ex00/book.py
+++ module_01/ex00/book.py
@@ -6,8 +6,69 @@
 #    By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/08 11:17:01 by plouvel           #+#    #+#              #
-#    Updated: 2023/03/08 11:19:15 by plouvel          ###   ########.fr        #
+#    Updated: 2023/03/08 15:29:56 by plouvel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+from datetime import datetime
+from recipe import Recipe
+
+
 class Book:
+    def __init__(self, name) -> None:
+        if not isinstance(name, str):
+            raise TypeError("name must be a string")
+        if not name:
+            raise ValueError("name cannot be empty")
+        self.creation_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        self.last_update = self.creation_date
+        self.recipes_list = {"starter": [], "lunch": [], "dessert": []}
+
+    def get_recipe_by_name(self, name) -> Recipe:
+        """Prints a recipe with the name \texttt{name} and returns the instance"""
+
+        for key in self.recipes_list:
+            for recipe in self.recipes_list[key]:
+                if recipe.name == name:
+                    return recipe
+        raise ValueError(f"recipe {name} doesn't exist")
+
+    def get_recipes_by_types(self, recipe_type) -> list:
+        """Get all recipe names for a given recipe_type"""
+
+        if not isinstance(recipe_type, str):
+            raise TypeError("recipe_type must be a string")
+        recipes = self.recipes_list.get(recipe_type, None)
+        if recipes == None:
+            raise ValueError("recipe_type must be either starter, lunch or dessert")
+        return [recipe.name for recipe in recipes]
+
+    def add_recipe(self, recipe) -> None:
+        """Add a recipe to the book and update last_update"""
+
+        if not isinstance(recipe, Recipe):
+            raise TypeError("recipe must be an instance of Recipe class")
+        if recipe.name in [r.name for r in self.recipes_list[recipe.recipe_type]]:
+            raise ValueError(f"recipe {recipe.name} already exist in that recipe type")
+        self.last_update = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        self.recipes_list[recipe.recipe_type].append(recipe)
+
+
+if __name__ == "__main__":
+    b = Book("Awesome book")
+    b.add_recipe(
+        Recipe(
+            "caca", 3, 60, ["foods"], "This is the result of human ingestion.", "lunch"
+        )
+    )
+    b.add_recipe(
+        Recipe(
+            "caca",
+            3,
+            60,
+            ["foods"],
+            "This is the result of human ingestion.",
+            "starter",
+        )
+    )
+    print(b.get_recipes_by_types("lunch"))
--- module_01/ex00/recipe.py
+++ module_01/ex00/recipe.py
@@ -6,7 +6,7 @@
 #    By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/08 11:19:20 by plouvel           #+#    #+#              #
-#    Updated: 2023/03/08 13:09:52 by plouvel          ###   ########.fr        #
+#    Updated: 2023/03/08 14:48:58 by plouvel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,20 +16,52 @@ class Recipe:
         self, name, cooking_lvl, cooking_time, ingredients, description, recipe_type
     ) -> None:
         if not isinstance(name, str):
-            raise TypeError("name must be a string.")
+            raise TypeError("name must be a string")
         if not name:
             raise ValueError("name cannot be empty")
 
         if not isinstance(cooking_lvl, int):
-            raise TypeError("cooking_lvl must be an integer.")
+            raise TypeError("cooking_lvl must be an integer")
         if cooking_lvl not in range(1, 6):
-            raise ValueError("cooking_lvl must be in range 1-5 included.")
+            raise ValueError("cooking_lvl must be in range 1-5 included")
 
+        if not isinstance(cooking_time, int):
+            raise TypeError("cooking_time must be an integer")
         if cooking_time < 1:
             raise ValueError("cooking_time must be a postive integer")
+
+        if not isinstance(ingredients, list):
+            raise TypeError("ingredients must be a list")
+        if not ingredients:
+            raise ValueError("ingredients cannot be empty")
+        for ingredient in ingredients:
+            if not isinstance(ingredient, str):
+                raise TypeError("each ingredient must be a string")
+
+        if not isinstance(recipe_type, str):
+            raise TypeError("recipe_type must be a string")
         if recipe_type not in ("starter", "lunch", "dessert"):
-            raise ValueError("recipe_type must be either start, lunch, or dessert.")
+            raise ValueError("recipe_type must be either start, lunch, or dessert")
+
+        self.name = name
+        self.cooking_lvl = cooking_lvl
+        self.cooking_time = cooking_time
+        self.ingredients = ingredients
+        self.description = description
+        self.recipe_type = recipe_type
+
+    def __str__(self):
+        return f"""\t\t-- Recipe for {self.name} --
+
+\tLevel of difficulty : {self.cooking_lvl}
+\tIngredients : {self.ingredients}
+\tCooking Time : {self.cooking_time}
+\tRecipe type : {self.recipe_type}
+\tDescription : {self.description}"""
 
 
 if __name__ == "__main__":
-    r = Recipe("caca", 2, 2, 2, 2, "startesr")
+    r = Recipe(
+        "caca", 3, 60, ["foods"], "This is the result of human ingestion.", "lunch"
+    )
+    print(str(r))
